544c0e1a65145b27d1ec1ff658b25bf4a4f79e6b
Harsh Singhal <harshparkar27@gmail.com>
2023-09-07 01:08:15 +0530

    executives added

--- src/components/Team.js
+++ src/components/Team.js
@@ -15,6 +15,11 @@ import Arun from '../images/team/arun.jpg';
 import Vaibhav from '../images/team/vaibhav.jpeg';
 import Rishav from '../images/team/Rishav.jpeg';
 import aryan_junior from '../images/team/aryan.jpg';
+import akash from '../images/team/akash.jpeg';
+import amit from '../images/team/amit.jpeg';
+import aman from '../images/team/aman.jpeg';
+import palak from '../images/team/palak.jpeg';
+import malay from '../images/team/malay.jpeg';
 
 const Team = () => {
   return (
@@ -110,7 +115,7 @@ const Team = () => {
                 </div>
               </div>
               <div className="member">Aryan Agarwal</div>
-              <p style={{ color: 'white' }}>Super Coordinator</p>
+              <p style={{ color: 'white' }}>Mentor</p>
             </div>
           </div>
           <div className="team-outerbox">
@@ -133,7 +138,7 @@ const Team = () => {
                 </div>
               </div>
               <div className="member">Ansaf Ahmad</div>
-              <p style={{ color: 'white' }}>Super Coordinator</p>
+              <p style={{ color: 'white' }}>Mentor</p>
             </div>
           </div>
           <div className="team-outerbox">
@@ -156,7 +161,7 @@ const Team = () => {
                 </div>
               </div>
               <div className="member">Arun Kumar</div>
-              <p style={{ color: 'white' }}>Super Coordinator</p>
+              <p style={{ color: 'white' }}>Mentor</p>
             </div>
           </div>
           <div className="team-outerbox">
@@ -295,6 +300,121 @@ const Team = () => {
               </div>
               <div className="member">Aryan Sagar</div>
               <p style={{ color: 'white' }}>Coordinator</p>
+            </div>
+          </div>
+          <div className="team-outerbox">
+            <div className="team-inner-box">
+              <div className="image-container">
+                <img src={amit} alt="" />
+                <div className="overlay">
+                  <div className="social-links-overlay">
+                    <a href="https://instagram.com/amitkaswan._?utm_source=qr&igshid=YzU1NGVlODEzOA==" target="_blank" rel="noopener noreferrer">
+                      <FontAwesomeIcon icon={faInstagram} />
+                    </a>
+                    <a href="https://www.linkedin.com/in/amit-kaswan-83895b286" target="_blank" rel="noopener noreferrer">
+                      <FontAwesomeIcon icon={faLinkedin} />
+                    </a>
+                    <a href="mailto:amitkaswan710@gmail.com" target="_blank" rel="noopener noreferrer">
+                      <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="envelope" class="svg-inline--fa fa-envelope " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"></path>
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="member">Amit Kaswan</div>
+              <p style={{ color: 'white' }}>Executive</p>
+            </div>
+          </div>
+          <div className="team-outerbox">
+            <div className="team-inner-box">
+              <div className="image-container">
+                <img src={palak} alt="" />
+                <div className="overlay">
+                  <div className="social-links-overlay">
+                    <a href="https://instagram.com/esportz.iitrpr/" target="_blank" rel="noopener noreferrer">
+                      <FontAwesomeIcon icon={faInstagram} />
+                    </a>
+                    <a href="www.linkedin.com/in/palak-hudda-87495528b" target="_blank" rel="noopener noreferrer">
+                      <FontAwesomeIcon icon={faLinkedin} />
+                    </a>
+                    <a href="mailto:palakhudda77@gmail.com" target="_blank" rel="noopener noreferrer">
+                      <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="envelope" class="svg-inline--fa fa-envelope " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"></path>
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="member">Palak</div>
+              <p style={{ color: 'white' }}>Executive</p>
+            </div>
+          </div>
+          <div className="team-outerbox">
+            <div className="team-inner-box">
+              <div className="image-container">
+                <img src={akash} alt="" />
+                <div className="overlay">
+                  <div className="social-links-overlay">
+                    <a href="https://instagram.com/akash_iit_ropar?utm_source=qr&igshid=MThlNWY1MzQwNA==" target="_blank" rel="noopener noreferrer">
+                      <FontAwesomeIcon icon={faInstagram} />
+                    </a>
+                    <a href="https://www.linkedin.com/in/akash-naik-akash-1a9368261" target="_blank" rel="noopener noreferrer">
+                      <FontAwesomeIcon icon={faLinkedin} />
+                    </a>
+                    <a href="mailto:akashakashnaik10@gmail.com" target="_blank" rel="noopener noreferrer">
+                      <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="envelope" class="svg-inline--fa fa-envelope " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"></path>
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="member">Sugali Akash naik</div>
+              <p style={{ color: 'white' }}>Executive</p>
+            </div>
+          </div>
+          <div className="team-outerbox">
+            <div className="team-inner-box">
+              <div className="image-container">
+                <img src={aman} alt="" />
+                <div className="overlay">
+                  <div className="social-links-overlay">
+                    <a href="https://www.instagram.com/amanjain.x?utm_source=qr&r=nametag" target="_blank" rel="noopener noreferrer">
+                      <FontAwesomeIcon icon={faInstagram} />
+                    </a>
+                    <a href="https://www.linkedin.com/in/aman-jain-88439b259" target="_blank" rel="noopener noreferrer">
+                      <FontAwesomeIcon icon={faLinkedin} />
+                    </a>
+                    <a href="mailto:amanjainbajoli2004@gmail.com" target="_blank" rel="noopener noreferrer">
+                      <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="envelope" class="svg-inline--fa fa-envelope " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"></path>
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="member">Aman Jain</div>
+              <p style={{ color: 'white' }}>Executive</p>
+            </div>
+          </div>
+          <div className="team-outerbox">
+            <div className="team-inner-box">
+              <div className="image-container">
+                <img src={malay} alt="" />
+                <div className="overlay">
+                  <div className="social-links-overlay">
+                    <a href="https://instagram.com/malay__jsr?utm_source=qr&igshid=YzU1NGVlODEzOA==" target="_blank" rel="noopener noreferrer">
+                      <FontAwesomeIcon icon={faInstagram} />
+                    </a>
+                    <a href="https://www.linkedin.com/in/malay-ayachit-504056240" target="_blank" rel="noopener noreferrer">
+                      <FontAwesomeIcon icon={faLinkedin} />
+                    </a>
+                    <a href="mailto:2023mmb1415@iitrpr.ac.in" target="_blank" rel="noopener noreferrer">
+                      <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="envelope" class="svg-inline--fa fa-envelope " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"></path>
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="member">Malay Ayachit</div>
+              <p style={{ color: 'white' }}>Executive</p>
             </div>
           </div>
         </div>
